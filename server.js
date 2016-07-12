9d558f9134c11f1bb98443a8dc3a59a32a4b6ee7
leptone <leptone55@gmail.com>
2016-07-11 20:50:50 -0700

    added signup form and route

--- server.js
+++ server.js
@@ -4,17 +4,37 @@ var app = express();
 var mongoose = require('./config/mongoose')
 var db = mongoose()
 
+var bodyParser = require('body-parser')
+
 var User = require('mongoose').model('User')
-var newUser = new User({
-    email: 'email',
-    password: 'password'
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res) {
+    res.render('signup')
 })
 
-
+app.post('/signup', function(req, res) {
+    var user = new User({
+        email: req.body.email,
+        password: req.body.password
+    })
+    user.save(function(err) {
+        if (err) throw err
+        console.log('Saved', user, 'in DB')
+    })
+})
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.use(express.static('./public'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
